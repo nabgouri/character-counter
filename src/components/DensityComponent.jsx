ae4c8185674c85d26c3ac46a852e8b77470294ef
ayman nabgorui <nabgouri9@gmail.com>
2025-04-23 17:22:48 +0100

    adding the show button

--- src/components/DensityComponent.jsx
+++ src/components/DensityComponent.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import LetterInfo from "./LetterInfo";
+import { ChevronDown, ChevronUp } from "lucide-react";
 export default function DensityComponent({ statsData }) {
   const { totalCharNum, totalChar } = statsData;
-  // const [letterDensity, setLetterDensity] = useState([]);
+  const [showMore, setShowMore] = useState(false);
+  
   const handleLetterDensity = () => { 
     const letterArr = totalChar.match(/[a-zA-Z]/g) || [];
-    console.log(letterArr)
     const letterDensity = {};
     letterArr.forEach((letter)=> {
       letterDensity[letter] = (letterDensity[letter] || 0) + 1
@@ -16,10 +17,16 @@ export default function DensityComponent({ statsData }) {
       
     }))
   };
-  const letterDensity = handleLetterDensity();
+  const letterDensity = handleLetterDensity().sort((a, b) => {
+    return b.count - a.count;
+  }
+  );
   
+  const handleSeeMore = () => {
+    setShowMore(!showMore);
+  };
   return (
-    <section>
+    <section className="mb-[64px]">
       <h2 className="text-text text-2xl/[130%] font-semibold mb-5">
         Letter Density
       </h2>
@@ -27,12 +34,28 @@ export default function DensityComponent({ statsData }) {
         <p className="text-text text-base/[130%]">
           No characters found. Start typing to see letter density.
         </p>
-      ) : (
-        <ul>
+      ) : letterDensity.length > 5?  (
+        <>
+          <ul>
+          {showMore ? letterDensity.map((letter, index) => {
+             return <LetterInfo key={index} totalChar={totalChar} letterCount={letter} />;
+          }) : letterDensity.slice(0,5).map((letter, index) => {
+               return <LetterInfo key={index} totalChar={totalChar} letterCount={letter} />;
+            })}
+          </ul>
+        <button className="text-text cursor-pointer mt-[20px] flex items-center gap-2 " onClick={handleSeeMore}>See {showMore ? 'Less'  : 'More'} { showMore ? <ChevronUp /> : <ChevronDown /> } </button>
+          
+        </>
+      ): (
+        <>
+          <ul>
           {letterDensity.map((letter, index) => {
              return <LetterInfo key={index} totalChar={totalChar} letterCount={letter} />;
           })}
-        </ul>
+                </ul>
+          
+        </>
+
       )}
     </section>
   );
